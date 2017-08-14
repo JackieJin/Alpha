5577def02cd3f75da37453ef744a4b34c114af69
JackieJin <qiang284107201@gmail.com>
2017-08-13 21:25:09 -0400

    further work on price_handler

--- core/price_handler.py
+++ core/price_handler.py
@@ -1,81 +1,65 @@
 import datetime
 
 import pandas as pd
+import numpy as np
 
 from data.data_factory import StockData
 from data.get_data import get_data_from_db
-from core.pool import EventPool
+from core.pool import PriceEventPool
+from core.event import PriceEvent
 
 class PriceHandler(object):
-    def __init__(self, data_symbols, init_tickers = None, start_date = None, end_date= None, freq = 'D'):
+    def __init__(self, data_symbols, init_tickers=[], start_date = None, end_date= None, freq = 'B'):
         self.data_symbols       = data_symbols
         self.start_date         = start_date
         self.end_date           = end_date
         self.freq               = freq
-        self.tickers            = {}
+        self.tickers            = []
+        self.last_price_events  = []
         self.init_tickers       = init_tickers
         self.data               = None
         self.timestamp          = pd.date_range(self.start_date, self.end_date, freq=self.freq)
-        self.curr_idx           = 0
+        self.curr_idx           = -1
         self.portfolio_handler  = None
 
-    def initialize(self, portfolio_handler):
+    def initialize(self, portfolio_handler=None):
         self._get_initial_data()
-        self.subscribe_tickers()
-        self.portfolio_handler      = portfolio_handler
+        self.portfolio_handler = portfolio_handler
+        timestamp = self.portfolio_handler.cur_time
+        if self.portfolio_handler is not None:
+            self.curr_idx = np.argmax(self.timestamp == timestamp)
+        self._subscribe_tickers(timestamp)
 
-    def get_last_timestamp(self, ticker):
+
+    def get_current_timestamp(self):
         """
         Returns the most recent actual timestamp for a given ticker
         """
-        if ticker in self.tickers:
-            timestamp = self.tickers[ticker]["timestamp"]
-            return timestamp
-        else:
-            print(
-                "Timestamp for ticker %s is not "
-                "available from the %s." % (ticker, self.__class__.__name__)
-            )
-            return None
+        return self.get_timestamp_by_idx(self.curr_idx)
 
-    def subscribe_ticker(self, ticker):
 
-        timestamp = self.timestamp[self.curr_idx]
-        try:
-            row             = self.data.loc[(timestamp, ticker), :]
-            close           = row['Close']
-            adj_close       = row['Adj Close']
-        except (KeyError,):
-            close       = None
-            adj_close   = None
 
-        ticker_prices = {
-            "close": close,
-            "adj_close": adj_close,
-            "timestamp": timestamp
-        }
-        self.tickers[ticker] = ticker_prices
-
-    def subscribe_tickers(self):
-        if self.init_tickers is not None:
-            for ticker in self.init_tickers:
-                self.subscribe_ticker(ticker)
-
-    def _get_initial_data(self):
-       self.data = get_data_from_db(self.data, self.data_symbols, self.start_date, self.end_date)
-
+    def get_timestamp_by_idx(self, idx):
+        timestamp = self.timestamp[idx]
+        return timestamp
 
     def stream_next(self):
-        event = EventPool(self.timestamp[self.curr_idx])
-        self.subscribe_tickers()
         self.curr_idx += 1
-        return event
+        if self.curr_idx < len(self.timestamp):
+            timestamp   = self.timestamp[self.curr_idx]
+            self._subscribe_tickers(timestamp)
+            price_events = self.tickers
+        else:
+            price_events = PriceEventPool(None)
+
+        return price_events
 
     def istick(self):
         return False
 
     def get_last_close(self, ticker):
-        close_price = self.tickers[ticker]["close"]
+        price_event = self.tickers.pool[ticker]
+        close_price = price_event.adj_price
         return close_price
 
     def continue_backtest(self):
@@ -83,6 +67,37 @@ class PriceHandler(object):
         if self.curr_idx >= len(self.timestamp):
             flag = False
         return flag
+
+
+
+    def _create_price_event(self, ticker, timestamp):
+        try:
+            row             = self.data.loc[timestamp, :]
+            adj_close       = row[ticker + '-Adj Close']
+            close           = row[ticker + '-Close']
+        except(KeyError,):
+            adj_close       = None
+            close           = None
+
+        if adj_close is None:
+            adj_close = self.tickers.pool[ticker].adj_price
+        if close is None:
+            close     = self.tickers.pool[ticker].price
+
+        event = PriceEvent(ticker, close, adj_close)
+        return event
+
+    def _subscribe_tickers(self, timestamp):
+
+        event_pool = PriceEventPool(timestamp)
+        for ticker in self.init_tickers:
+            event = self._create_price_event(ticker, timestamp)
+            event_pool.add(event)
+        self.last_price_events  = self.tickers
+        self.tickers            = event_pool
+
+    def _get_initial_data(self):
+        self.data = get_data_from_db(self.data, self.data_symbols, self.start_date, self.end_date)
 
 
 if __name__ == "__main__":
